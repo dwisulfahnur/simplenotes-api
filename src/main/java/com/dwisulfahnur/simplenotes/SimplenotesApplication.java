e2c27cfba5d3cd259e7d99d39a8d12347af448ea
Dwi Sulfahnur <hello@dwisulfahnur.com>
2023-05-05 10:51:40 +0700

    feat: add main app runner spring

---
+++ src/main/java/com/dwisulfahnur/simplenotes/SimplenotesApplication.java
@@ -0,0 +1,13 @@
+package com.dwisulfahnur.simplenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesApplication.class, args);
+	}
+
+}
